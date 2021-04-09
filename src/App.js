5ceeb6ee715b1278d381a77a372c6936c04e5db4
tfsn <tfsn86@gmail.com>
2021-04-09 16:06:24 +0200

    Created a navigation bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,35 @@ import './App.css';
 function App() {
 	return (
 		<Fragment>
-			<h1>Put content here</h1>
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">
+					Nu Går Vi
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<a className="nav-link" href="#">
+							Om
+						</a>
+						<a className="nav-link" href="#">
+							Log ind
+						</a>
+						<a className="nav-link" href="#">
+							Tilmeld
+						</a>
+					</div>
+				</div>
+			</nav>
 		</Fragment>
 	);
 }
