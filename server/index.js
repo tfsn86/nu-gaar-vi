77729ebae761193e4a04fc25aefcd4c47ce064ce
tfsn <tfsn86@gmail.com>
2021-05-06 07:14:20 +0200

    Updated API with CRUD operations (PUT, POST and DELETE)

--- server/index.js
+++ server/index.js
@@ -35,6 +35,56 @@ app.get('/steps', async (req, res) => {
 	}
 });
 
+// Get a step count
+app.get('/steps/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const step = await pool.query(
+			'SELECT * FROM stepstable WHERE step_id = $1',
+			[id]
+		);
+
+		res.json(step.rows[0]);
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+// Update a step count
+app.put('/steps/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { steps } = req.body;
+
+		// eslint-disable-next-line
+		const updateStep = await pool.query(
+			'UPDATE stepstable SET steps = $1 WHERE step_id = $2',
+			[steps, id]
+		);
+
+		res.json(`Step count on step_id ${id} was updated!`);
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
+// Delete a step count
+app.delete('/steps/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		// eslint-disable-next-line
+		const deleteStep = await pool.query(
+			'DELETE FROM stepstable WHERE step_id = $1',
+			[id]
+		);
+
+		res.json(`Step count on step_id ${id} was deleted!`);
+	} catch (error) {
+		console.error(error.message);
+	}
+});
+
 app.listen(5000, () => {
 	console.log('Server has started on port 5000');
 });
