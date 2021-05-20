5431a9f86819afefd11a8f344233e13d9352114e
tfsn <tfsn86@gmail.com>
2021-05-20 20:34:20 +0200

    minor changes

--- db.js
+++ db.js
@@ -2,16 +2,16 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 // const devConfig = {
-// 	user: process.env.PG_USER,
-// 	password: process.env.PG_PASSWORD,
-// 	host: process.env.PG_HOST,
-// 	port: process.env.PG_PORT,
-// 	database: process.env.PG_DATABASE,
+//   user: process.env.PG_USER,
+//   password: process.env.PG_PASSWORD,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   port: process.env.PG_PORT,
 // };
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; // heroku addons
+const proConfig = process.env.DATABASE_URL; //heroku addons
 
 const pool = new Pool({
 	connectionString:
