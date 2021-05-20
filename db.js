4d642c6d7ab115650a7b7e7c52d8fb56c61954a5
tfsn <tfsn86@gmail.com>
2021-05-20 21:31:02 +0200

    Trying to fix database connection problem

--- db.js
+++ db.js
@@ -11,13 +11,12 @@ require('dotenv').config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-console.log('Database_URL', process.env.DATABASE_URL);
 const proConfig = process.env.DATABASE_URL + '?sslmode=require'; //heroku addons
-console.log(proConfig);
 
 const pool = new Pool({
 	connectionString:
 		process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+	ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
