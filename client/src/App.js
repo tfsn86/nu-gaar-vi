f09778022609e9eba9c145c08bdc78c25e0d39fb
tfsn <tfsn86@gmail.com>
2021-07-28 19:38:36 +0200

    UI auth added to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Fragment, useState, useEffect } from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
 
 import Header from './components/Header';
 import Landing from './components/Landing';
@@ -9,17 +15,80 @@ import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 
 function App() {
+	const checkAuthenticated = async () => {
+		try {
+			const res = await fetch('http://localhost:5000/auth/verify', {
+				method: 'POST',
+				headers: { jwt_token: localStorage.token },
+			});
+
+			const parseRes = await res.json();
+
+			parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	useEffect(() => {
+		checkAuthenticated();
+	}, []);
+
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	const setAuth = (boolean) => {
+		setIsAuthenticated(boolean);
+	};
 	return (
-		<Router>
-			<Header />
-			<Switch>
-				<Route exact path="/" component={Landing} />
-				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/about" component={About} />
-				<Route path="/signin" component={UserSignIn} />
-				<Route path="/signup" component={UserSignUp} />
-			</Switch>
-		</Router>
+		<Fragment>
+			<Router>
+				<Header />
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={(props) =>
+							!isAuthenticated ? (
+								<Landing {...props} />
+							) : (
+								<Redirect to="/dashboard" />
+							)
+						}
+					/>
+					<Route
+						path="/signin"
+						render={(props) =>
+							!isAuthenticated ? (
+								<UserSignIn {...props} setAuth={setAuth} />
+							) : (
+								<Redirect to="/dashboard" />
+							)
+						}
+					/>
+					<Route
+						path="/signup"
+						render={(props) =>
+							!isAuthenticated ? (
+								<UserSignUp {...props} setAuth={setAuth} />
+							) : (
+								<Redirect to="/dashboard" />
+							)
+						}
+					/>
+					<Route path="/about" render={About} />
+					<Route
+						path="/dashboard"
+						render={(props) =>
+							isAuthenticated ? (
+								<Dashboard {...props} setAuth={setAuth} />
+							) : (
+								<Redirect to="/login" />
+							)
+						}
+					/>
+				</Switch>
+			</Router>
+		</Fragment>
 	);
 }
 
