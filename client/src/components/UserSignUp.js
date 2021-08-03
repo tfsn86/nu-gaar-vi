e7ca883f457584e25b196c0a54cfb40a20e9dbfc
tfsn <tfsn86@gmail.com>
2021-08-03 16:09:40 +0200

    Fixing fetching issue

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -16,7 +16,7 @@ const UserSignUp = ({ setAuth }) => {
 		e.preventDefault();
 		try {
 			const body = { email, password, name };
-			const response = await fetch('http://localhost:5000/auth/register', {
+			const response = await fetch('/auth/register', {
 				method: 'POST',
 				headers: {
 					'Content-type': 'application/json',
