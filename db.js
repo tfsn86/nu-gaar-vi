0b266381672b1fbbf8c2bba7f6c62a56f1300c49
tfsn <tfsn86@gmail.com>
2021-09-05 22:04:36 +0200

    Changes to SSL config

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
-const ssl = '?ssl=require';
+const ssl = '?sslmode=no-verify';
 
 const pool = new Pool({
 	connectionString:
