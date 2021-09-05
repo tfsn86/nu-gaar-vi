ac6d28e208b89008cdf324d35b4c6bad9b3aee97
tfsn <tfsn86@gmail.com>
2021-09-05 22:00:22 +0200

    Experimenting with SSL config in production

--- db.js
+++ db.js
@@ -12,14 +12,15 @@ require('dotenv').config();
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
+const ssl = '?ssl=require';
 
 const pool = new Pool({
 	connectionString:
-		process.env.NODE_ENV === 'production' ? proConfig : devConfig,
-	ssl: {
-		sslmode: 'require',
-		rejectUnauthorized: false,
-	},
+		process.env.NODE_ENV === 'production' ? proConfig + ssl : devConfig,
+	// ssl: {
+	// 	sslmode: 'require',
+	// 	rejectUnauthorized: false,
+	// },
 });
 
 module.exports = pool;
