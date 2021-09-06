a49bb0939fbb217da1e16e9502b8df71175a3681
tfsn <tfsn86@gmail.com>
2021-09-06 13:55:40 +0200

    Minor changes to DB SSL configuration, comments and git repository

--- db.js
+++ db.js
@@ -1,26 +1,16 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-// const devConfig = {
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PASSWORD,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   port: process.env.PG_PORT,
-// };
-
+// Development configuration
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
+// Production configuration
 const proConfig = process.env.DATABASE_URL; //heroku addons
 const ssl = '?sslmode=no-verify';
 
 const pool = new Pool({
 	connectionString:
 		process.env.NODE_ENV === 'production' ? proConfig + ssl : devConfig,
-	// ssl: {
-	// 	sslmode: 'require',
-	// 	rejectUnauthorized: false,
-	// },
 });
 
 module.exports = pool;
