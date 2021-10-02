3e646ffe840d77d072375087bd6cbd2f9b282dc0
tfsn <tfsn86@gmail.com>
2021-10-02 09:07:07 +0200

    Redirect users to the SSL version of the app in production

--- index.js
+++ index.js
@@ -5,23 +5,18 @@ const pool = require('./db');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-// process.env.PORT
-// process.env.NODE_ENV => production or undefined
+// enable ssl redirect
+app.use(sslRedirect());
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // req.body
-
-// app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === 'production') {
 	//server static content
 	// npm run build
 	app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
-console.log(__dirname);
-console.log(path.join(__dirname, 'client/build'));
 
 // Routes
 app.use('/auth', require('./routes/auth'));
